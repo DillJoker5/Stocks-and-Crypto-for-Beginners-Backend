d3449b49d3427b455650db34ccc81557c54eb035
Dylan Chirigotis <dylan.chirigotis@mail.wlc.edu>
2022-04-30 13:14:44 -0500

    Finished first three creation endpoints

--- models/responses.go
+++ models/responses.go
@@ -4,6 +4,13 @@ import (
 	"time"
 )
 
+type Response struct {
+	UserId int32 `json: "user_id"`
+	ThreadId int32 `json: "thread_id"`
+	Description string `json: "description"`
+	DateCreated time.Time `json: "date_created"`
+}
+
 type ResponseTable struct {
 	ResponseId int32 `json: "response_id"`
 	UserId int32 `json: "user_id"`
